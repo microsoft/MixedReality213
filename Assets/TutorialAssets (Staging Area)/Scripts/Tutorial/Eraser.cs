ded718b6ae97d45b46be3d348274d48aea9aec46
Lars Simkins <lars.simkins@gmail.com>
2017-10-03 12:39:21 -0700

    Added eraser jitter

--- Assets/TutorialAssets (Staging Area)/Scripts/Tutorial/Eraser.cs
+++ Assets/TutorialAssets (Staging Area)/Scripts/Tutorial/Eraser.cs
@@ -18,20 +18,29 @@ namespace MRDL.ControllerExamples
                 for (int i = brushStrokes.Count - 1; i >= 0; i--)
                 {
                     // Do a crude check for proximity with the brush stroke's render bounds
-                    Bounds strokeBounds = brushStrokes[i].GetComponent<Renderer>().bounds;
-                    if (strokeBounds.Contains (TipPosition))
+                    LineRenderer lineRenderer = brushStrokes[i].GetComponent<LineRenderer>();
+                    if (erasedStrokes.Contains(lineRenderer))
+                        continue;
+
+                    if (lineRenderer.bounds.Contains (TipPosition))
                     {
                         // If we're in bounds, check whether any point of the stroke is within range
-                        LineRenderer lineRenderer = brushStrokes[i].GetComponent<LineRenderer>();
                         Vector3[] positions = new Vector3[lineRenderer.positionCount];
                         lineRenderer.GetPositions(positions);
                         for (int j = 0; j < positions.Length; j++)
                         {
-                            if (Vector3.Distance (positions[j], TipPosition) < eraseRange)
+                            if (Vector3.Distance(positions[j], TipPosition) < eraseRange)
                             {
-                                // Erase the brush stroke and remove it from our array
-                                GameObject.Destroy(brushStrokes[i]);
+                                // Un-tag and erase the brush stroke
+                                brushStrokes[i].tag = "Untagged";
                                 brushStrokes.RemoveAt(i);
+                                erasedStrokes.Enqueue(lineRenderer);
+                                if (!erasingStrokes)
+                                {
+                                    // If we've just added a new stroke, start erasing them
+                                    erasingStrokes = true;
+                                    StartCoroutine(EraseStrokesOverTime());
+                                }
                                 break;
                             }
                         }
@@ -42,7 +51,46 @@ namespace MRDL.ControllerExamples
             yield break;
         }
 
+        private IEnumerator EraseStrokesOverTime()
+        {
+            while (erasedStrokes.Count > 0)
+            {
+                LineRenderer lineRenderer = erasedStrokes.Dequeue();
+                float startTime = Time.unscaledTime;
+                float startWidth = lineRenderer.widthMultiplier;
+                // Get the positions from the line renderer
+                Vector3[] startPositions = new Vector3[lineRenderer.positionCount];
+                lineRenderer.GetPositions(startPositions);
+                // Create a new array for their positions as they're being erased
+                Vector3[] endPositions = new Vector3[lineRenderer.positionCount];
+                while (Time.unscaledTime < startTime + eraseTime)
+                {
+                    float normalizedTime = (Time.unscaledTime - startTime) / eraseTime;
+                    for (int i = 0; i < startPositions.Length; i++)
+                    {
+                        // Add a bit of random noise based on how far away the point is
+                        Vector3 randomNoise = Random.insideUnitSphere * Vector3.Distance(endPositions[i], TipPosition) * 0.1f;
+                        endPositions[i] = Vector3.Lerp(startPositions[i], TipPosition, normalizedTime) + randomNoise;
+                        lineRenderer.SetPositions(endPositions);
+                        // Make the line skinnier as we go
+                        lineRenderer.widthMultiplier = Mathf.Lerp(startWidth, startWidth * 0.1f, normalizedTime);
+                    }
+                    yield return null;
+                }
+                GameObject.Destroy(lineRenderer.gameObject);
+                yield return null;
+            }
+
+            erasingStrokes = false;
+            yield break;
+        }
+
         [SerializeField]
         private float eraseRange = 0.1f;
+        [SerializeField]
+        private float eraseTime = 0.15f;
+
+        private bool erasingStrokes = false;        
+        private Queue<LineRenderer> erasedStrokes = new Queue<LineRenderer>();
     }
 }
