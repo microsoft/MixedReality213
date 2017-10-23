76cb5791d196c7f6ae741f5f139353123b0f43b5
Katherine Harris <kvm.harris@gmail.com>
2017-10-23 16:30:54 -0700

    Adding ToolTips. However, there is an error on play.

---
+++ Assets/ToolTips/ControllerToolTipGroupManager.cs
@@ -0,0 +1,499 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MRDL.ToolTips;
+
+public class ControllerToolTipGroupManager : MonoBehaviour {
+
+    public const int MaxToolTipsPerGroup = 20;
+    public const int MaxToolTipGroups = 10;
+
+    public enum PositionModeEnum
+    {
+        Automatic,  // 'Boquet' system
+        Manual,     // Manually enter a local position
+    }
+
+    public enum DisplayModeEnum
+    {
+        On,
+        Off,
+        OnFocus,
+        None
+    }
+
+    #region classes
+    [Serializable]
+    public struct ToolTipTemplate
+    {
+        // Has the developer entered a name for this tooltip?
+        public bool IsEmpty
+        {
+            get
+            {
+                return string.IsNullOrEmpty(Text);
+            }
+        }
+
+        // Do we have all the components we need?
+        public bool IsSetUp
+        {
+            get
+            {
+                return Anchor != null;
+            }
+        }
+
+        public string Text;
+        public PositionModeEnum PositionMode;
+        [Tooltip("Object that tooltip will attach to")]
+        public Transform Anchor;
+        public float LineLength;
+        public Vector3 PivotPoint;
+        public Vector3 LocalPosition;
+        //[NonSerialized]
+        //[HideInInspector]
+        public GameObject AssociatedGameObject;
+        // TBD Profiles
+    }
+
+    [Serializable]
+    public class ToolTipGroup
+    {
+        public string GroupName;
+        public DisplayModeEnum GroupDisplayMode;
+        public ToolTipTemplate[] ToolTips = new ToolTipTemplate[MaxToolTipsPerGroup];
+        public int visibleToolTipsCount = 0;
+        public GameObject groupPrefab;
+    }
+    #endregion
+
+
+    public void NextGroup()
+    {
+        int newGroupIndex = groupIndex;
+
+        newGroupIndex++;
+        if (newGroupIndex >= groups.Count)
+            newGroupIndex = groups.Count - 1;
+
+        if (newGroupIndex != groupIndex)
+        {
+            groupIndex = newGroupIndex;
+
+            RefreshCurrentGroup();
+
+        }
+    }
+
+    public void PrevGroup()
+    {
+        int newGroupIndex = groupIndex;
+
+        newGroupIndex--;
+        if (newGroupIndex < 0)
+            newGroupIndex = 0;
+
+        if (newGroupIndex != groupIndex)
+        {
+            groupIndex = newGroupIndex;
+            RefreshCurrentGroup();
+
+        }
+    }
+
+    public ToolTipGroup GetGroup(int index)
+    {
+        return groups[index];
+    }
+
+    public ToolTipGroup CurrentGroup
+    {
+        get
+        {
+            if (groupIndex >= groups.Count || groupIndex < 0)
+                return null;
+
+            return groups[groupIndex];
+        }
+    }
+
+
+    public int NumGroups
+    {
+        get
+        {
+            return groups.Count;
+        }
+    }
+
+    public int CurrentGroupIndex
+    {
+        get
+        {
+            return groupIndex;
+        }
+    }
+
+    public void DeactivateCurrentGroup()
+    {
+        for (int i = 0; i < currentToolTips.Length; i++)
+        {
+            currentToolTips[i].gameObject.SetActive(false);
+        }
+    }
+
+    #region editor tools
+    //   #if UNITY_EDITOR
+
+    public void GoToGroup(int newGroupIndex)
+    {
+        if (newGroupIndex >= groups.Count)
+            newGroupIndex = groups.Count - 1;
+
+        if (newGroupIndex < 0)
+            newGroupIndex = 0;
+
+        if (newGroupIndex != groupIndex)
+        {
+            // Deactivate Current Group
+            // DeactivateCurrentGroup();
+            groupIndex = newGroupIndex;
+
+            RefreshCurrentGroup();
+
+        }
+    }
+
+    public void GoToGroup(string groupName)
+    {
+        int newGroupIndex = groupIndex;
+        for (int i = 0; i < groups.Count; i++)
+        {
+            // TODO enforce GroupName in editor
+            if (groups[i].GroupName.ToLower() == groupName.ToLower())
+            {
+                newGroupIndex = i;
+                break;
+            }
+        }
+
+        if (newGroupIndex != groupIndex)
+        {
+            groupIndex = newGroupIndex;
+
+            RefreshCurrentGroup();
+        }
+    }
+
+
+    public void RefreshCurrentGroup()
+    {
+        if (CurrentGroup == null)
+            return;
+
+        if (!Application.isPlaying)
+            return;
+
+        for (int i = 0; i < CurrentGroup.ToolTips.Length; i++)
+        {
+            if (!CurrentGroup.ToolTips[i].IsEmpty)
+            {
+                if (!CurrentGroup.ToolTips[i].IsSetUp)
+                {
+                    Debug.LogError("Tool tip " + i.ToString() + " is not set up, skipping");
+                    currentToolTips[i].gameObject.SetActive(false);
+                }
+                else
+                {
+                    currentToolTips[i].ToolTipText = CurrentGroup.ToolTips[i].Text;
+                    currentToolTips[i].PivotPosition = CurrentGroup.ToolTips[i].PivotPoint;
+                    currentToolTips[i].Anchor = CurrentGroup.ToolTips[i].Anchor.gameObject;
+
+                }
+            }
+            else
+            {
+                // Place tooltip according to Position Mode
+                // Make Active based on State Mode
+
+            }
+        }
+    }
+
+    public void AddGroup(string groupName)
+    {
+        ToolTipGroup group = new ToolTipGroup();
+        group.GroupName = groupName;
+        // Set the line length to 1 for reach template
+        for (int i = 0; i < group.ToolTips.Length; i++)
+        {
+            group.ToolTips[i].LineLength = 1;
+        }
+        groups.Add(group);
+    }
+
+    public void RemoveGroup(int index)
+    {
+        if (index < groups.Count)
+            groups.RemoveAt(index);
+    }
+
+
+
+    public void AddToolTip(int index)
+    {
+        CurrentGroup.visibleToolTipsCount++;
+        if (CurrentGroup.visibleToolTipsCount == CurrentGroup.ToolTips.Length)
+        {
+            // Make array larger and transfer everything over to larger array
+        }
+        else
+        {
+            CurrentGroup.ToolTips[index] = new ToolTipTemplate();
+        }
+
+    }
+
+    public void ActivateGroup(string groupName)
+    {
+
+    }
+
+    public void DeactivateGroup(string groupName)
+    {
+
+    }
+
+    public void ActivateGroup(int groupIndex)
+    {
+        for (int i = 0; i < groups[groupIndex].ToolTips.Length; i++)
+        {
+
+        }
+    }
+
+    public void DeactivateGroup(int groupIndex)
+    {
+
+    }
+
+    //#endif
+    #endregion
+
+    // Use this for initialization
+    void Awake()
+    {
+        for (int i = 0; i < groups.Count; i++)
+        {
+            for (int j = 0; j < groups[i].ToolTips.Length; j++)
+            {
+                if (!(groups[i].ToolTips[j].IsEmpty))
+                {
+                    GameObject toolTipGo = GameObject.Instantiate(groups[i].groupPrefab, transform) as GameObject;
+                    // Immediately Deactivate if not in active group
+                    ToolTip toolTip = toolTipGo.GetComponent<ToolTip>();
+                    toolTip.ToolTipText = groups[i].ToolTips[j].Text;
+                    toolTip.PivotPosition = groups[i].ToolTips[j].PivotPoint;
+                    toolTip.Anchor.transform.position = groups[i].ToolTips[j].Anchor.position;
+
+
+                    groups[i].ToolTips[j].AssociatedGameObject = toolTipGo;
+                }
+            }
+            if (groups[i].GroupDisplayMode == DisplayModeEnum.None)
+                GroupDisplayNone(groups[i], i);
+            //toolTip.GroupTipState = ToolTip.TipDisplayModeEnum.None;
+
+            if (groups[i].GroupDisplayMode == DisplayModeEnum.On)
+                GroupDisplayOn(groups[i], i);
+            //toolTip.GroupTipState = ToolTip.TipDisplayModeEnum.On;
+
+            if (groups[i].GroupDisplayMode == DisplayModeEnum.Off)
+                GroupDisplayOff(groups[i], i);
+
+            //toolTip.GroupTipState = ToolTip.TipDisplayModeEnum.Off;
+
+            if (groups[i].GroupDisplayMode == DisplayModeEnum.OnFocus)
+                GroupDisplayOnFocus(groups[i], i);
+
+            //toolTip.GroupTipState = ToolTip.TipDisplayModeEnum.OnFocus;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Updating");
+        //if (CurrentGroup.GroupDisplayMode == DisplayModeEnum.On)
+        //{
+        //    GroupDisplayOn();
+        //}
+        //if (CurrentGroup.GroupDisplayMode == DisplayModeEnum.Off)
+        //{
+        //    Debug.Log("OGG");
+        //    GroupDisplayOff();
+        //}
+        //if (CurrentGroup.GroupDisplayMode == DisplayModeEnum.None)
+        //{
+        //    GroupDisplayNone();
+        //}
+    }
+
+    public void GroupDisplayModeChanged(ToolTipGroup group, DisplayModeEnum X, int index)
+    {
+        Debug.Log("Got here");
+        switch (X)
+        {
+            case DisplayModeEnum.None:
+                GroupDisplayNone(group, index);
+
+                break;
+            case DisplayModeEnum.Off:
+                GroupDisplayOff(group, index);
+
+                break;
+            case DisplayModeEnum.On:
+                GroupDisplayOn(group, index);
+                break;
+            case DisplayModeEnum.OnFocus:
+                GroupDisplayOnFocus(group, index);
+
+                break;
+
+        }
+    }
+
+    public void GroupDisplayOn(ToolTipGroup group, int index)
+    {
+        for (int i = 0; i < group.ToolTips.Length; i++)
+        {
+            if (group.ToolTips[i].AssociatedGameObject != null)
+                group.ToolTips[i].AssociatedGameObject.GetComponent<ToolTip>().GroupTipState = ToolTip.TipDisplayModeEnum.On;
+
+        }
+    }
+
+    public void GroupDisplayOff(ToolTipGroup group, int index)
+    {
+        for (int i = 0; i < group.ToolTips.Length; i++)
+        {
+            if (group.ToolTips[i].AssociatedGameObject != null)
+                group.ToolTips[i].AssociatedGameObject.GetComponent<ToolTip>().GroupTipState = ToolTip.TipDisplayModeEnum.Off;
+
+        }
+    }
+
+    public void GroupDisplayOnFocus(ToolTipGroup group, int index)
+    {
+        for (int i = 0; i < group.ToolTips.Length; i++)
+        {
+            if (group.ToolTips[i].AssociatedGameObject != null)
+                group.ToolTips[i].AssociatedGameObject.GetComponent<ToolTip>().GroupTipState = ToolTip.TipDisplayModeEnum.OnFocus;
+
+        }
+    }
+
+    public void GroupDisplayNone(ToolTipGroup group, int index)
+    {
+        for (int i = 0; i < group.ToolTips.Length; i++)
+        {
+            if (group.ToolTips[i].AssociatedGameObject != null)
+                group.ToolTips[i].AssociatedGameObject.GetComponent<ToolTip>().GroupTipState = ToolTip.TipDisplayModeEnum.None;
+
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+            return;
+
+        Gizmos.color = Color.cyan;
+
+        Vector3 anchorPosition = Vector3.zero;
+        Vector3 startPosition = Vector3.zero;
+        Vector3 direction = Vector3.zero;
+        Vector3 endPosition = Vector3.zero;
+
+        if (CurrentGroup != null)
+        {
+            // Draw crude lines and cubes for each tool tip so we can preview what they'll be like
+            foreach (ToolTipTemplate template in CurrentGroup.ToolTips)
+            {
+                if (!template.IsEmpty && template.IsSetUp)
+                {
+                    switch (template.PositionMode)
+                    {
+                        case PositionModeEnum.Automatic:
+                            anchorPosition = template.Anchor.position;
+                            startPosition = template.Anchor.position;
+                            direction = (startPosition - template.Anchor.position).normalized;
+                            endPosition = anchorPosition + (direction * template.LineLength * lineLength);
+                            Gizmos.DrawLine(startPosition, endPosition);
+                            // TODO - make the cube the size of the tooltip 
+                            Gizmos.DrawCube(endPosition, new Vector3(0.025f, 0.025f, 0.025f));
+                            break;
+
+                        case PositionModeEnum.Manual:
+                            startPosition = template.Anchor.position;
+                            endPosition = transform.TransformPoint(template.LocalPosition);
+                            Gizmos.DrawLine(startPosition, endPosition);
+                            // TODO - make the cube the size of the tooltip
+                            Gizmos.DrawCube(endPosition, Vector3.one);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    // The prefab we'll use to create tooltips
+    [SerializeField]
+    private GameObject toolTipPrefab;
+
+    public GameObject ToolTipPrefab
+    {
+        set
+        {
+            toolTipPrefab = value;
+        }
+        get
+        {
+            return toolTipPrefab;
+        }
+    }
+
+    private GameObject[] prefabList;
+
+    // The length of the lines attached to tooltips
+    // This will be multiplied by the template's line length
+    [SerializeField]
+    private float lineLength = 1f;
+
+    // If position mode is set to automatic
+    // his will be used as the point from which tooltips will spread outward
+    [SerializeField]
+    private Transform OriginTransform;
+
+    [SerializeField]
+    private int groupIndex = 0;
+
+    // The group - keeping this 
+    [SerializeField]
+    private List<ToolTipGroup> groups = new List<ToolTipGroup>();
+
+    public List<ToolTipGroup> GetGroups
+    {
+        get
+        {
+            return groups;
+        }
+    }
+
+    // The current set of tooltips
+    private ToolTip[] currentToolTips = new ToolTip[MaxToolTipsPerGroup];
+}
+
---
+++ Assets/ToolTips/Editor/ControllerToolTipGroupManagerInspector.cs
@@ -0,0 +1,239 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+namespace MRDL.ToolTips
+{
+    [CustomEditor(typeof(ControllerToolTipGroupManager))]
+    public class ControllerToolTipGroupManagerInspector : Editor {
+
+        public override void OnInspectorGUI()
+        {
+            ControllerToolTipGroupManager ttgm = (ControllerToolTipGroupManager)target;
+
+            if (ttgm.NumGroups == 0)
+                ttgm.AddGroup("New Group");
+
+
+            GUI.color = Color.white;
+            EditorGUILayout.LabelField("Groups", EditorStyles.whiteLargeLabel);
+
+
+            // [<-] [ + Group ] [->]
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("+ Group"))
+            {
+                ttgm.AddGroup("New Group");
+            }
+            EditorGUILayout.EndHorizontal();
+
+            // [ little group editor ]
+            // [ little group editor ]
+            // [ little group editor ]
+            EditorGUILayout.BeginVertical();
+            for (int i = 0; i < ttgm.NumGroups; i++)
+            {
+                bool delete = false;
+                if (DrawLittleGroupEditor(ttgm.GetGroup(i).GroupName, i == ttgm.CurrentGroupIndex, ref delete, ttgm.GetGroup(i)))
+                {
+                    ttgm.GoToGroup(i);
+                }
+                if (delete)
+                    ttgm.RemoveGroup(i);
+
+            }
+            EditorGUILayout.EndVertical();
+
+
+            DrawBigGroupEditor(ttgm.CurrentGroup, ttgm);
+
+            GUI.color = Color.white;
+            if (GUILayout.Button("Add Tool Tip"))
+            {
+                int index = ttgm.CurrentGroup.visibleToolTipsCount + 1;
+                ttgm.AddToolTip(index);
+            }
+
+        }
+
+        private bool DrawLittleGroupEditor(string GroupName, bool isCurrentGroup, ref bool delete, ControllerToolTipGroupManager.ToolTipGroup group)
+        {
+            bool clicked = false;
+
+
+            // [ Group name ] [X]
+            EditorGUILayout.BeginHorizontal();
+            GUI.color = isCurrentGroup ? Color.yellow : Color.white;
+            EditorGUILayout.LabelField(group.GroupDisplayMode.ToString(), GUILayout.MaxWidth(150));
+            if (GUILayout.Button(GroupName))
+            {
+                clicked = true;
+            }
+            GUI.color = Color.red;
+            if (GUILayout.Button("X", GUILayout.MaxWidth(25)))
+            {
+                delete = true;
+            }
+            EditorGUILayout.EndHorizontal();
+            GUI.color = Color.white;
+            return clicked;
+        }
+
+        private void DrawBigGroupEditor(ControllerToolTipGroupManager.ToolTipGroup group, ControllerToolTipGroupManager ttgm)
+        {
+            GUI.color = Color.white;
+            EditorGUILayout.LabelField("Current Group: " + group.GroupName, EditorStyles.whiteLargeLabel);
+            EditorGUILayout.BeginHorizontal();
+            group.GroupName = EditorGUILayout.TextField(group.GroupName);
+            EditorGUILayout.EndHorizontal();
+            group.groupPrefab = (GameObject)EditorGUILayout.ObjectField("Tool Tip prefab", group.groupPrefab, typeof(GameObject), false);
+            if (group.groupPrefab == null)
+            {
+                GUI.color = Color.red;
+                EditorGUILayout.LabelField("Warning: You need to set a prefab");
+
+            }
+            //ttgm.ToolTipPrefab = (GameObject)EditorGUILayout.ObjectField("Tool Tip prefab", ttgm.ToolTipPrefab, typeof(GameObject), false);
+
+            //if (ttgm.ToolTipPrefab == null)
+            //{
+            //    GUI.color = Color.red;
+            //    EditorGUILayout.LabelField("Warning: You need to set a prefab");
+
+            //}
+            // [ tooltip template ]
+            // [ tooltip template ]
+            // [ tooltip template ]
+            EditorGUILayout.BeginVertical();
+            ControllerToolTipGroupManager.DisplayModeEnum X = group.GroupDisplayMode;
+            group.GroupDisplayMode = (ControllerToolTipGroupManager.DisplayModeEnum)EditorGUILayout.EnumPopup("Display Mode", group.GroupDisplayMode);
+            if (group.GroupDisplayMode != X)
+            {
+                ttgm.GroupDisplayModeChanged(group, group.GroupDisplayMode, ttgm.CurrentGroupIndex);
+            }
+
+            EditorGUILayout.LabelField("Tool tips", EditorStyles.whiteLargeLabel);
+            if (group.visibleToolTipsCount < group.ToolTips.Length)
+            {
+                for (int i = 0; i < group.visibleToolTipsCount; i++)
+                {
+                    group.ToolTips[i] = DrawTemplateEditor(group.ToolTips[i]);
+                    EditorGUILayout.Space();
+                }
+
+            }
+            else
+            {
+                // create new group or longer list if longer
+            }
+
+            EditorGUILayout.EndVertical();
+
+        }
+
+        private ControllerToolTipGroupManager.ToolTipTemplate DrawTemplateEditor(ControllerToolTipGroupManager.ToolTipTemplate template)
+        {
+            GUI.color = Color.white;
+            /*if (template.IsEmpty)
+            {
+                GUI.color = Color.gray;
+            }*/
+            /*else if (!template.IsSetUp)
+            {
+                GUI.color = Color.red;
+                EditorGUILayout.LabelField("Warning: There are fields missing");
+            }*/
+
+            // [ "ToolTip Text:" ] [ (text input) ] [ (clear button) ]
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("ToolTip Text:", GUILayout.MaxWidth(75));
+            template.Text = EditorGUILayout.TextField(template.Text);
+            if (GUILayout.Button("Clear"))
+            {
+                template.Text = string.Empty;
+            }
+            EditorGUILayout.EndHorizontal();
+
+            // if it's empty just show the name, nothing else
+            if (!template.IsEmpty)
+            {
+                template.PositionMode = (ControllerToolTipGroupManager.PositionModeEnum)EditorGUILayout.EnumPopup("Position mode", template.PositionMode);
+                switch (template.PositionMode)
+                {
+                    case ControllerToolTipGroupManager.PositionModeEnum.Automatic:
+                    default:
+                        template.LineLength = EditorGUILayout.Slider("Line length", template.LineLength, .01f, 10f);
+                        break;
+
+                    case ControllerToolTipGroupManager.PositionModeEnum.Manual:
+                        template.LocalPosition = EditorGUILayout.Vector3Field("Local position (manual)", template.LocalPosition);
+                        break;
+                }
+                //bool 
+                //if(EditorGUILayout.Toggle)
+                template.Anchor = (Transform)EditorGUILayout.ObjectField("Anchor object", template.Anchor, typeof(Transform), true);
+            }
+            return template;
+
+        }
+
+        #region SceneEditorTools
+        public enum ObjectModeEnum
+        {
+            Object,
+            Vector
+        }
+
+        static ObjectModeEnum ObjectMode = ObjectModeEnum.Object;
+        static int selectedVectorIndex = 0;
+        static int selectedObjectIndex = 0;
+
+        Color unselectedColor = Color.Lerp(Color.cyan, Color.clear, 0.95f);
+        Color selectedColor = Color.Lerp(Color.yellow, Color.clear, 0.95f);
+        Color lineColorObject = Color.white;
+        Color lineColorVector = Color.red;
+        Color inactiveColor = Color.Lerp(Color.gray, Color.clear, 0.99f);
+
+        protected virtual void OnSceneGUI()
+        {
+            ControllerToolTipGroupManager example = (ControllerToolTipGroupManager)target;
+
+            EditorGUI.BeginChangeCheck();
+            // draw handles on objects
+            Debug.Log("Length of List " + example.CurrentGroup.ToolTips.Length);
+            Debug.Log("Length of Visible " + example.CurrentGroup.visibleToolTipsCount);
+            for (int i = 0; i < example.CurrentGroup.visibleToolTipsCount; i++)
+            {
+                ControllerToolTipGroupManager.ToolTipTemplate connectedObject = example.CurrentGroup.ToolTips[i];
+
+                if (Handles.Button(connectedObject.PivotPoint, Quaternion.identity, 0.05f, 0.05f, Handles.CubeHandleCap))
+                {
+                    selectedObjectIndex = i;
+                }
+
+                if (i == selectedObjectIndex)
+                {
+                    Handles.color = selectedColor;
+                    connectedObject.PivotPoint = Handles.PositionHandle(connectedObject.PivotPoint, Quaternion.identity);
+                }
+                else
+                {
+                    Handles.color = unselectedColor;
+                }
+
+                Handles.color = Color.Lerp(Handles.color, lineColorVector, 0.5f);
+                if (connectedObject.Anchor != null && connectedObject.PivotPoint != null)
+                {
+                    Handles.DrawLine(connectedObject.Anchor.transform.position, connectedObject.PivotPoint);
+                }
+                Handles.Label(connectedObject.PivotPoint + Vector3.up * 0.25f, "Object " + i.ToString());
+
+                example.CurrentGroup.ToolTips[i] = connectedObject;
+            }
+            EditorGUI.EndChangeCheck();
+        }
+        #endregion
+    }
+}
