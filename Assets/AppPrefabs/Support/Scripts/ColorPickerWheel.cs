6552d057fcf4be4629cf9bce3f9ebda02cb06a2d
REDMOND\dongpark <dongpark@microsoft.com>
2017-11-20 12:59:15 -0800

    Applied PR #1392 from MRTK - Updated namespace,

--- Assets/AppPrefabs/Support/Scripts/ColorPickerWheel.cs
+++ Assets/AppPrefabs/Support/Scripts/ColorPickerWheel.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using HoloToolkit.Unity.Controllers;
+using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 using UnityEngine.XR.WSA.Input;
 
